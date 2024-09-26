377c05f6c37a68cd8957a766d7166d0ababb238e
przuljpe <petar.przulj@mail.utoronto.ca>
2024-09-26 16:17:46 -0400

    finished activity 1.4

--- activity_1_4.py
+++ activity_1_4.py
@@ -18,16 +18,19 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    session["uoft"] = True
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get("name")
+        old_email = session.get("email")
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you changed your name!")
+        if old_email is not None and old_email != form.email.data:
+            flash("Looks like you changed your email!")
         session["name"] = form.name.data
         session["email"] = form.email.data
         return redirect(url_for('index'))
-
-    return render_template("index.html", form=form, name=session.get('name'), email=session.get('email'))
+    return render_template("index.html", form=form, name=session.get('name'), email=session.get('email'), uoft=session.get('uoft'))
 
 if __name__ == "__main__":
     app.run()
